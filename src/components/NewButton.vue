75158379a510eecc134b77280c25fabcc57d0122
unknown <bouibkeroussama@hotmail.fr>
2021-11-26 19:47:04 +0100

    NewButton + CreateProject

---
+++ src/components/NewButton.vue
@@ -0,0 +1,91 @@
+<template>
+    <nav>
+        <ul>
+            <li>{{ title }}
+                <span></span><span></span><span></span><span></span>
+                </li>
+        </ul>
+    </nav>
+      
+</template>
+
+<script>
+export default {
+  name: 'NewButton',
+  props: {
+    title: String,
+  },
+  
+}
+</script>
+
+<style lang="css" scoped>
+
+nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+nav ul li {
+    --c: rgb(90, 90, 235);
+    color: var(--c);
+    font-size: 16px;
+    border: 0.3em solid var(--c);
+    border-radius: 0.5em;
+    width: 12em;
+    height: 3em;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-family: sans-serif;
+    letter-spacing: 0.1em;
+    text-align: center;
+    line-height: 3em;
+    position: relative;
+    overflow: hidden;
+    z-index: 1;
+    transition: 0.5s;
+    margin: 1em;
+    padding-bottom:3em;
+}
+
+nav ul li span {
+    position: absolute;
+    width: 25%;
+    height: 100%;
+    background-color: var(--c);
+    transform: translateY(150%);
+    border-radius: 50%;
+    left: calc((var(--n) - 1) * 25%);
+    transition: 0.5s;
+    transition-delay: calc((var(--n) - 1) * 0.1s);
+    z-index: -1;
+}
+
+nav ul li:hover {
+    color: white;
+}
+
+nav ul li:hover span {
+    transform: translateY(0) scale(2);
+}
+
+nav ul li span:nth-child(1) {
+    --n: 1;
+}
+
+nav ul li span:nth-child(2) {
+    --n: 2;
+}
+
+nav ul li span:nth-child(3) {
+    --n: 3;
+}
+
+nav ul li span:nth-child(4) {
+    --n: 4;
+}
+
+
+
+</style>
